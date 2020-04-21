8b86077afcbfc86a55bb37e57b768bba5d46ef22
康雪烛 <62123890+Heniz19210601@users.noreply.github.com>
2020-04-21 14:49:39 +0800

    Update #100352 跳水板.py

--- #100352 跳水板.py
+++ #100352 跳水板.py
@@ -1,12 +1,12 @@
 shorter = 1
 longer = 2
 k = 3
-l = []
-while len(l) < k+1:
+l = []##所有随机出的总长度列表
+while len(l) < k+1:##所有长度的可能性是k+1种
     length = 0
-    for i in range(k):
-        length = length + random.randint(shorter,longer)
+    for i in range(k):##选取k根木板
+        length = length + random.randint(shorter,longer)##从长短里随机选取两个相加即得跳板的总长度
     if length not in l:
-        l.append(length)
-l.sort()
+        l.append(length)##如果计算出的长度不在列表里则添加变成一个新的可能性
+l.sort()##从小到大排列
 print(l)
