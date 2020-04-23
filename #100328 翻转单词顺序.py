f084e0251a1cb7285c097fc375355f30719fee4d
康雪烛 <62123890+Heniz19210601@users.noreply.github.com>
2020-04-23 16:25:05 +0800

    Update and rename 翻转单词顺序.py to #100328 翻转单词顺序.py

---
+++ #100328 翻转单词顺序.py
@@ -0,0 +1,8 @@
+input = 'the sky is blue'
+a = input.split()##去掉所有空格
+b = b[::-1]##翻转String内元素的顺序
+c = ''##一个空的String
+for i in b:
+    c = c + i + ' '##添加元素后都加一个空格
+c.rstrip()##移除开头和结尾的多余空格
+print(c)
--- 翻转单词顺序.py
+++
@@ -1,7 +0,0 @@
-input = 'the sky is blue'
-a = input.split()
-c = ''
-b = b[::-1]
-for i in b:
-    c = c + i + ' '
-print(c)
